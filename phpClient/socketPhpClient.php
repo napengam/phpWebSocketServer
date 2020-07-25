0240f94ab4ef7253d865f7adcb65a008d059ddba
Heinz <Heinz@Heinz-PC.fritz.box>
2020-07-25 11:56:29 +0200

    prpend  __DIR__  to include path

--- phpClient/socketPhpClient.php
+++ phpClient/socketPhpClient.php
@@ -6,22 +6,19 @@ class socketTalk {
     private $socketMaster;
 
     function __construct($Address, $Port, $application = '/') {
-        $secure = false;
-        $arr = explode('://', $Address);
+        $context = stream_context_create();
+        $arr = explode('://', $Address, 2);
         if (count($arr) > 1) {
             if (strncasecmp($arr[0], 'ssl', 3) == 0) {
-                $secure = true;
+                stream_context_set_option($context, 'ssl', 'allow_self_signed', true);
+                stream_context_set_option($context, 'ssl', 'verify_peer', false);
+                stream_context_set_option($context, 'ssl', 'verify_peer_name', false);
             } else {
                 $Address = $arr[1]; // just the host
             }
         }
-        $context = stream_context_create();
-        if ($secure) {
-            $context = stream_context_create();
-            stream_context_set_option($context, 'ssl', 'allow_self_signed', true);
-            stream_context_set_option($context, 'ssl', 'verify_peer', false);
-            stream_context_set_option($context, 'ssl', 'verify_peer_name', false);
-        }
+        $errno = 0;
+        $errstr = '';
         $this->socketMaster = stream_socket_client("$Address:$Port", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $context);
 
         if (!$this->socketMaster) {
@@ -70,7 +67,7 @@ class socketTalk {
         }
         fwrite($this->socketMaster, $m);
         $buff = fread($this->socketMaster, 256); // wait for ACK
-        $ack=json_decode($buff);
+        $ack = json_decode($buff);
         if ($ack->opcode != 'next') {
             $this->silent();
             return false;
