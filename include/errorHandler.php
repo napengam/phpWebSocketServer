c5b653cbbca31aa628c3a267ee438a28798092bd
Heinz <Heinz@Heinz-PC.fritz.box>
2020-09-04 09:51:18 +0200

    errorHandler

---
+++ include/errorHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+set_error_handler('errorException');
+
+if (function_exists('errorException')) {
+    return true;
+}
+
+function errorException($errno, $errstr, $errfile, $errline) {
+    global $logger;
+    if (!(error_reporting() & $errno)) {
+        return true;
+    }
+    $logger->log("errno: $errno ; $errstr in file $errfile , Line: $errline");
+    return true;
+}
