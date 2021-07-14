83da75f703f5f5b5495205c00cec2ee99caa59bd
Heinz <Heinz@Heinz-PC.fritz.box>
2021-07-14 10:16:24 +0200

    connects to websocket server at piesocket.com

---
+++ websocketOrg/websocketPie.php
@@ -0,0 +1,24 @@
+<?php
+
+include __DIR__ . "/../phpClient/websocketCore.php";
+
+class websocketPie extends websocketCore {
+
+    public $uuid, $connected = false, $chunkSize = 6 * 1024;
+
+    //private $socketMaster;
+
+    function __construct($Address, $Port = '', $app = '/', $uu = '') {
+
+        if (parent::__construct($Address, $Port, $app, $uu) == false) {
+            return;
+        }
+       
+        $respo = $this->readSocket();
+        echo $respo;
+    }
+
+}
+
+$x = new websocketPie("ssl://demo.piesocket.com",'',"/v3/1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm");
+
