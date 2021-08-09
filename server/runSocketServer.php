c8b0ff5a313b0340eaf4ec882930ce811712ecf6
Heinz <Heinz@Heinz-PC.fritz.box>
2021-08-09 16:21:02 +0200

    refactored code

--- server/runSocketServer.php
+++ server/runSocketServer.php
@@ -1,91 +1,103 @@
 <?php
 
-/*
- * ***********************************************
- * the runtime
- * ***********************************************
- */
-include __DIR__ . '/../include/certPath.inc.php';
-include __DIR__ . '/../include/adressPort.inc.php';
-include __DIR__ . '/../include/logToFile.inc.php';
-include __DIR__ . '/../include/errorHandler.php';
-include __DIR__ . '/logToFile.php';
-/*
- * ***********************************************
- * inlcude the core server
- * ***********************************************
- */
-include __DIR__ . "/getOptions.php";
-include __DIR__ . "/webSocketServer.php";
-/*
- * **********************************************
- *  your backend applications
- * **********************************************
- */
-include __DIR__ . '/resource.php';
-include __DIR__ . '/resourceDefault.php';
-include __DIR__ . '/resourceWeb.php';
-include __DIR__ . '/resourcePHP.php';
+class runSocketServer {
 
-/*
- * ***********************************************
- * check for parameters 
- * ***********************************************
- */
+    function __construct() {
+        /*
+         * ***********************************************
+         * the runtime
+         * ***********************************************
+         */
+        require __DIR__ . '/errorHandler.php';
+        require __DIR__ . '/logToFile.php';
+        /*
+         * ***********************************************
+         * inlcude the core server
+         * ***********************************************
+         */
+        require __DIR__ . "/getOptions.php";
+        require __DIR__ . "/webSocketServer.php";
+        /*
+         * **********************************************
+         *  your backend applications
+         * **********************************************
+         */
+        require __DIR__ . '/resource.php';
+        require __DIR__ . '/resourceDefault.php';
+        require __DIR__ . '/resourceWeb.php';
+        require __DIR__ . '/resourcePHP.php';
+    
+    }
 
-$o = new getOptions();
-$option = $o->default;
-/*
- * ***********************************************
- * create a logger
- * set directory for logfiles and 
- * log to console
- * ***********************************************
- */
+    function run() {
+        /*
+         * ***********************************************
+         * check for parameters 
+         * ***********************************************
+         */
 
-$logger = new logToFile(dirname($option['logfile']), $option['console']);
-if ($logger->error === '') {
-    $logger->logOpen(basename($option['logfile']));
-} else {
-    $logger = '';
-    openlog('websock', LOG_PID, LOG_USER); 
-    syslog(LOG_ERR, "can not create loging with ". $option['logfile']);
-    closelog();
+        $o = new getOptions();
+        $option = $o->default;
+        /*
+         * ***********************************************
+         * create a logger
+         * set directory for logfiles and 
+         * log to console
+         * ***********************************************
+         */
+
+        $logger = new logToFile(dirname($option['logfile']), $option['console']);
+        if ($logger->error === '') {
+            $logger->logOpen(basename($option['logfile']));
+        } else {
+            $logger = '';
+            openlog('websock', LOG_PID, LOG_USER);
+            syslog(LOG_ERR, "can not create loging with " . $option['logfile']);
+            closelog();
+        }
+        /*
+         * *****************************************
+         * create server 
+         * *****************************************
+         */
+        $server = new websocketServer($option['adress'], $logger, $option['certFile'], $option['pkFile']);
+        /*
+         * ***********************************************
+         * set some server variables
+         * ***********************************************
+         */
+        $server->maxPerIP = 0;   // 0=unlimited 
+        /*
+         * ***********************************************
+         * instantiate backend 'applications'
+         * ***********************************************
+         */
+        $resDefault = new resourceDefault();
+        $resWeb = new resourceWeb();
+        $resPHP = new resourcePHP();
+        /*
+         * ***********************************************
+         * register backend 'applications' with server
+         * ***********************************************
+         */
+        $server->registerResource('/', $resDefault);
+        $server->registerResource('/web', $resWeb);
+        $server->registerResource('/php', $resPHP);
+        /*
+         * ***********************************************
+         * now start it to have the server handle
+         * requests from clients
+         * ***********************************************
+         */
+
+        $server->Start();
+    }
+
 }
 /*
- * *****************************************
- * create server 
- * *****************************************
- */
-$server = new websocketServer($option['adress'], $logger, $option['certFile'], $option['pkFile']);
-/*
- * ***********************************************
- * set some server variables
- * ***********************************************
- */
-$server->maxPerIP = 0;   // 0=unlimited 
-/*
- * ***********************************************
- * instantiate backend 'applications'
- * ***********************************************
- */
-$resDefault = new resourceDefault();
-$resWeb = new resourceWeb();
-$resPHP = new resourcePHP();
-/*
- * ***********************************************
- * register backend 'applications' with server
- * ***********************************************
- */
-$server->registerResource('/', $resDefault);
-$server->registerResource('/web', $resWeb);
-$server->registerResource('/php', $resPHP);
-/*
- * ***********************************************
- * now start it to have the server handle
- * requests from clients
- * ***********************************************
- */
-
-$server->Start();
-
+************************************************
+* start 
+************************************************
+*/
+$s = new runSocketServer();
+$s->run();
