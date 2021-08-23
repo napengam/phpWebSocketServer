a3c04549d7e1610b6a152da7121476fbf64a5315
Heinz <Heinz@Heinz-PC.fritz.box>
2021-08-23 06:21:00 +0200

    server will send more then 8192 bytes

--- websocketExtern/websocketxrpl.php
+++ websocketExtern/websocketxrpl.php
@@ -9,9 +9,25 @@ class websocketXrpl extends websocketCore {
         if (parent::__construct($Address) == false) {
             return;
         }
-        $this->writeSocket(' {"command": "server_info"} ');
-        $respo = $this->readSocket();
-        echo $respo;
+//        $this->writeSocket(' {"command": "server_info"} ');
+//        $resp1 = $this->readSocket();
+        //secho $resp1;
+        $InitialTXLookup = json_encode(array(
+            'id' => 2,
+            'command' => "account_tx",
+            'account' => "r4DymtkgUAh2wqRxVfdd3Xtswzim6eC6c5",
+            'ledger_index_min' => -1,
+            'ledger_index_max' => -1,
+            'binary' => false,
+            'limit' => 50,
+            'forward' => true
+        ));
+
+        $this->writeSocket($InitialTXLookup);
+
+        $resp1 = $this->readSocket();
+        echo $resp1;
+        
     }
 
 }
